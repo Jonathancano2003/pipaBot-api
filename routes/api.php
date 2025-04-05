95258aa4a38361eb338f355369cb89825d9aba1a
Jonathancano2003 <124157297+Jonathancano2003@users.noreply.github.com>
2025-04-05 02:27:57 +0200

    afd

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GeminiController;
 use App\Http\Controllers\AuthController;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::post('/gemini/receive', [GeminiController::class, 'receiveMessage']);
 Route::post('/gemini/reset', [GeminiController::class, 'resetChat']);
-
+Route::post('/gemini/update-prompt', [GeminiController::class, 'updatePrompt']);
 Route::post('/login', [AuthController::class, 'login']);
